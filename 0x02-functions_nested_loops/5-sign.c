1a1a50a31a7c7c0b1d178ef3564dc5153fec41e0
UcenELma <elma.hocine@gmail.com>
2023-09-12 16:36:22 -0700

    alx task 0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,11 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: The number to check
+ * @n: the int to check
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ * Return: 1 and prints + if n is greater than zero
+ *         0 and prints 0 if n is zero
+ *        -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -18,9 +20,9 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
 	}
+	return (-1);
 }
